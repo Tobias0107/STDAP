835e91a2d4beee380ed72d18798466f44df5433d
Tobias van den Bosch <tobias.van.den.bosch@student.uva.nl>
2026-04-03 16:08:37 +0200

    Some testing osmnx  and combining / representing data

--- Project-Overview/Testing_stuff/test_OSMNX/osmnx_test.py
+++ Project-Overview/Testing_stuff/test_OSMNX/osmnx_test.py
@@ -2,13 +2,17 @@ import networkx as nx
 import osmnx as ox
 # print(ox.__version__)
 
-Name = "Almere"
+Name = "groningen"
 
+# Not entirely sure why we do this, but the tutorial says it is best
 ox.settings.bidirectional_network_types += "drive"
+
+# Create graph of all car-accessible roads in the city specified by 'Name'
 G = ox.graph_from_place(f"{Name}, Netherlands", simplify=True, network_type="drive")
 
-ox.plot_graph(G, save=True, show=True, close=False, filepath=f'Graphs/{Name}.png')
+# Project the graph to the Coordinate Reference System (CRS) used by the CBS
+G_proj = ox.project_graph(G, to_crs="epsg:28992", to_latlong=False)
 
-
-
+# Plot the graph, show the plot and save at the Graphs directory
+ox.plot_graph(G_proj, save=True, show=True, close=False, filepath=f'./Graphs/{Name}.png')
 
